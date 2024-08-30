bee9e9b67590d57fb3fb487751fa93b32e3d8746
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-08-30 11:36:46 +0300

    Shopping list logic working.

--- app/T4Ostoslista.jsx
+++ app/T4Ostoslista.jsx
@@ -1,22 +1,51 @@
+import { useState } from "react";
 import { FlatList, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function T4Ostoslista() {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const handlePress = (buttonKey) => {
+
+        switch (buttonKey) {
+            case "add":
+                if (todo.trim() !== "") {
+                    let newTodoEntry = { id: Date.now().toString(), todo: todo }
+                    setTodos([newTodoEntry, ...todos])
+                    setTodo("");
+                }
+                break;
+            case "clear":
+                setTodos([]);
+                setTodo("");
+                break;
+        }
+    }
+    console.log("Todos: ", todos);
+    console.log("Todo: ", todo);
 
 
     return (
         <SafeAreaProvider>
             <SafeAreaView style={styles.safeArea}>
                 <View style={styles.containerBasic}>
-                    <TextInput style={styles.input} />
+                    <TextInput
+                        style={styles.input}
+                        value={todo}
+                        onChangeText={text => setTodo(text)} />
                     <View style={styles.containerButtons}>
-                        <Pressable style={styles.buttonRounded}>
+                        <Pressable
+                            style={styles.buttonRounded}
+                            onPress={() => handlePress("add")}>
                             <Text style={styles.textButtons}>
                                 Add
                             </Text>
                         </Pressable>
-                        <Pressable style={styles.buttonRounded}>
+                        <Pressable
+                            style={styles.buttonRounded}
+                            onPress={() => handlePress("clear")}>
                             <Text style={styles.textButtons}>
                                 Clear
                             </Text>
@@ -26,8 +55,11 @@ export default function T4Ostoslista() {
                         <Text style={styles.textList}>
                             Shopping List
                         </Text>
-                        <FlatList>
-
+                        <FlatList
+                            data={todos}
+                            keyExtractor={(item) => item.id}
+                            renderItem={({ item }) => <Text style={styles.textResult}>{item.todo}</Text>}
+                            ListEmptyComponent={<Text></Text>}>
                         </FlatList>
                     </View>
                 </View>
@@ -50,11 +82,9 @@ const styles = StyleSheet.create({
         justifyContent: "flex-start",
         padding: 40
     },
-    containerResult: {
-        alignItems: "center"
-    },
     containerList: {
         marginTop: 40,
+        alignItems: "center"
     },
     input: {
         height: 40,
@@ -73,8 +103,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     textResult: {
-        fontSize: 20,
-        fontWeight: 'bold'
+        fontSize: 20
     },
     textButtons: {
         color: 'white',
